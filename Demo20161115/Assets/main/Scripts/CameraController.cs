383a93930327e9a252909dff7878b89f561a3de9
superDaYe <664319109@qq.com>
2017-12-28 13:15:53 +0800

    初始化

---
+++ Demo20161115/Assets/main/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    //要跟随的目标
+    public GameObject target;
+    //初始位置
+    private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+        //记录摄像机的初始位置
+        offset = transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
---
+++ Demo20161115/Assets/main/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private int count;//分数
+    public Text countText;
+    // Use this for initialization
+    void Start () {
+        count = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Console.WriteLine("moveHorizontal");
+    }
+
+    //触发器进入时触发
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            Debug.Log("OnTriggerEnter");
+        }
+    }
+    //碰撞器进入时触发
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "PickUp")
+        {
+            collision.gameObject.SetActive(false);
+            count++;
+            updateCountText();
+            Debug.Log("OnCollisionEnter");
+        }
+    }
+
+    private void updateCountText() {
+        countText.text = "Count:" + count;
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+       // Debug.Log("moveHorizontal: " + moveHorizontal);
+       // Debug.Log("moveVertical:" + moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal,0.0f, moveVertical);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+}
