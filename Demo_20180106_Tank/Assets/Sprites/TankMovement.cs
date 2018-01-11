f6e0474deef9fdd095825aa3eb33adccefd08020
superDaYe <664319109@qq.com>
2018-01-12 00:04:35 +0800

    增加左右方向和两个玩家控制

--- Demo_20180106_Tank/Assets/Sprites/TankMovement.cs
+++ Demo_20180106_Tank/Assets/Sprites/TankMovement.cs
@@ -7,6 +7,9 @@ public class TankMovement : MonoBehaviour
 
     public float speed = 5;
 
+    public float aApeed = 10;// 每秒10度
+
+    public float num = 1;
     private Rigidbody rigidbody;
 
     // Use this for initialization
@@ -23,12 +26,15 @@ public class TankMovement : MonoBehaviour
     private void FixedUpdate()
     {   // 表示垂直键（前后键）,vertical 在-1到1之间，这里的vertical是一个渐进的值，可以用这个特点做类似加速度之类的效果，来区分一下不同类型的坦克
         // 或者设计车重，在根据发动机功率计算加速度。
-        float vertical = Input.GetAxis("Vertical");
+        float vertical = Input.GetAxis("VerticalPlayer" + num);
+        //Debug.Log("v=" + vertical);
 
         // 自身向前的方向 * 按键的 * 速度
         rigidbody.velocity = this.transform.forward * vertical * speed;
 
-
-
+        float h = Input.GetAxis("HorizontalPlayer" + num);
+        //Debug.Log("h=" + h);
+        // 旋转速度为 围绕Y轴（up）旋转
+        rigidbody.angularVelocity = this.transform.up * aApeed * h;
     }
 }
