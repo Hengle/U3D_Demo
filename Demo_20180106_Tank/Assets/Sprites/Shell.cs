769070bde9b3162beb63f091917f6d67ae97a55d
superDaYe <664319109@qq.com>
2018-01-18 00:51:45 +0800

    子弹爆炸

---
+++ Demo_20180106_Tank/Assets/Sprites/Shell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+
+
+    public GameObject e;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        GameObject.Instantiate(e, this.transform.position, this.transform.rotation);
+        GameObject.Destroy(this.gameObject);
+    }
+}
