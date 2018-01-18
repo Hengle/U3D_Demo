17cd9dca4fa822232b111d18a38252eacae4f630
superDaYe <664319109@qq.com>
2018-01-18 20:50:49 +0800

    子弹飞行中让弹头始终朝下

---
+++ Demo_20180106_Tank/Assets/Sprites/SelfDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+
+    // 生存时间
+    public float time = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Destroy(this.gameObject, this.time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Demo_20180106_Tank/Assets/Sprites/Shell.cs
+++ Demo_20180106_Tank/Assets/Sprites/Shell.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Shell : MonoBehaviour
 {
@@ -17,7 +19,23 @@ public class Shell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        double xRota = 0;// 子弹在x轴上的旋转角度
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        Vector3 v = rigidbody.velocity;
+        //Debug.Log("x=" + v.y);
+        double tanf = v.y / Math.Pow((Math.Pow(v.x, 2.0) + Math.Pow(v.z, 2.0)), 0.5);
+        //Debug.Log("tan=" + tanf);
+        xRota = Mathf.Atan((float)tanf) * 180 / Math.PI;
+        //Debug.Log("f=" + xRota);
 
+        // this.transform.rotation = Quaternion.AngleAxis((float)xRota, Vector3.up);
+        Quaternion q = this.transform.rotation;
+        Vector3 qv = q.eulerAngles;
+        qv.x = (float)xRota;
+        this.transform.rotation = Quaternion.Euler(-qv);
+
+        //Debug.Log("x="+ q.z);
+        //q[0] = (float)xRota;
     }
 
     private void OnTriggerEnter(Collider other)
