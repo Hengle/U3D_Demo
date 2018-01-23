38c419efda6ffab64326dad9a2848c6e174e4a59
superDaYe <664319109@qq.com>
2018-01-24 00:01:35 +0800

    1

--- Demo_20180106_Tank/Assets/Sprites/Shell.cs
+++ Demo_20180106_Tank/Assets/Sprites/Shell.cs
@@ -21,18 +21,18 @@ public class Shell : MonoBehaviour
     {
         double xRota = 0;// 子弹在x轴上的旋转角度
         Rigidbody rigidbody = this.GetComponent<Rigidbody>();
-        Vector3 v = rigidbody.velocity;
+        Vector3 v = rigidbody.velocity;//获取子弹的当前速度
         //Debug.Log("x=" + v.y);
-        double tanf = v.y / Math.Pow((Math.Pow(v.x, 2.0) + Math.Pow(v.z, 2.0)), 0.5);
+        double tanf = v.y / Math.Pow((Math.Pow(v.x, 2.0) + Math.Pow(v.z, 2.0)), 0.5);// 计算速度的tan值
         //Debug.Log("tan=" + tanf);
-        xRota = Mathf.Atan((float)tanf) * 180 / Math.PI;
+        xRota = Mathf.Atan((float)tanf) * 180 / Math.PI;// 通过tan值和反正切函数计算角度
         //Debug.Log("f=" + xRota);
 
         // this.transform.rotation = Quaternion.AngleAxis((float)xRota, Vector3.up);
-        Quaternion q = this.transform.rotation;
-        Vector3 qv = q.eulerAngles;
-        qv.x = (float)-xRota;
-        this.transform.rotation = Quaternion.Euler(qv);
+        Quaternion q = this.transform.rotation;// 子弹的角度的四元素
+        Vector3 qv = q.eulerAngles;// 转换成3元
+        qv.x = (float)-xRota;// 设置以X为轴的旋转度数
+        this.transform.rotation = Quaternion.Euler(qv);// 转换成4元
 
         //Debug.Log("x="+ q.z);
         //q[0] = (float)xRota;
@@ -43,5 +43,12 @@ public class Shell : MonoBehaviour
 
         GameObject.Instantiate(e, this.transform.position, this.transform.rotation);
         GameObject.Destroy(this.gameObject);
+
+        if (other.tag == "Tank")
+        {
+            other.SendMessage("TankD");
+
+        }
+
     }
 }
