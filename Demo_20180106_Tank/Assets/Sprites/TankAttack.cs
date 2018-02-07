ad4f97526d8ab326e179524e7ab4f937a8c58a37
superDaYe <664319109@qq.com>
2018-02-08 01:40:04 +0800

    加了蓄力条

--- Demo_20180106_Tank/Assets/Sprites/TankAttack.cs
+++ Demo_20180106_Tank/Assets/Sprites/TankAttack.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankAttack : MonoBehaviour
 {
@@ -11,14 +12,28 @@ public class TankAttack : MonoBehaviour
     public GameObject shell;
 
     public float shellSpeed = 10;
+    // 发射的蓄力滑块
+    public Slider aimSlider;
+    // 最大力度
+    public float maxPower = 30f;
+    // 最小力度
+    public float minPower = 15f;
 
+    // 子弹的力的大小
+    private float shellPower;
 
     public KeyCode keycode = KeyCode.Space;
+
+
+    private void OnEnable()
+    {
+        shellPower = minPower;
+        aimSlider.value = minPower;
+    }
 
     // Use this for initialization
     void Start()
     {
-
         point = this.transform.Find("point");
 
 
@@ -26,13 +41,16 @@ public class TankAttack : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         // 开火
         if (Input.GetKeyDown(keycode))
         {
             GameObject zidan = GameObject.Instantiate(shell, point.position, point.rotation);
             Rigidbody rig = zidan.GetComponent<Rigidbody>();
             rig.velocity = this.point.forward * shellSpeed;
+
+
+            aimSlider.value += 1;
 
         }
 
